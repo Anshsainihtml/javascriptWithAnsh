63c86c8fbc80269be62180e65e6198ae96e9cdfb
anshsaini <anshsainihtml@gmail.com>
2025-06-07 21:44:27 +0530

    console.log, error,table, warn, assert

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello console");
+console.log(4+3);
+console.log(true);
+console.log([34,2,1,2]);
+console.log({name: "ansh", marks: 34});
+console.table({name: "ansh", marks: 34});
+console.warn("this is a worning");
+// console.clear();
+console.assert(99>189, "Age > 189 is not possible");
+
+console.error("This is a error");
+
+/* this 
+is a
+multiline comment 
+*/
+
+
